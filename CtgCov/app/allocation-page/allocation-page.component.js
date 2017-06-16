6608314e32a69d9dc9bef3bbdfb0e25503e856a6
rohit <rohit.nandamudi@just-roar.com>
2017-06-16 22:56:16 +0530

    adding functionality to components like yard table and allocation

--- CtgCov/app/allocation-page/allocation-page.component.js
+++ CtgCov/app/allocation-page/allocation-page.component.js
@@ -10,6 +10,14 @@ var core_1 = require("@angular/core");
 var AllocationPageComponent = (function () {
     function AllocationPageComponent() {
         this.name = 'AllocationPageComponent';
+        this.data = {
+            rows: [
+                ["Available", "2343443", "05-20-2017 08:30", "Chattanooga ", "Walmart", "Atlanta,GA", "Buford,GA", "SOLO", "2342345", "SID", "all set",],
+                ["Available", "2343444", "05-20-2017 09:30", "West depot", "Fedex", "Ontario OH", "Bufford,GA", "SOLO", "2342345", "SID", "all set",],
+                ["Available", "2343445", "05-20-2017 10:30", "CTG ", "VH Transport", "Bentonville,AR", "Bufford,GA", "SOLO", "2342345", "SID", "all set",]
+            ],
+            column: ["Status", "Order#", "Order date", "Origin name", "Bill to name", "Origin city", "Destination city", "Service", "Ref#", "Ref type", "Order remark", "Action"]
+        };
     }
     return AllocationPageComponent;
 }());
--- CtgCov/app/table/table.component.ts
+++ CtgCov/app/table/table.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
-    selector: 'table',
+    selector: 'app-table',
     templateUrl: 'table.component.html',
     styleUrls: [ 'table.component.css'],
     moduleId: module.id
 })
 export class TableComponent {
     private name = 'TableComponent';
+    @Input('master') master: string;
+    @Input() user: string;
+    @Input() config: JSON;
 }
 
 // This code copy to app.module.ts
