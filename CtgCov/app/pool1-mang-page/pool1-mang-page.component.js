702c74870b7ed0ba57782d5280c8cad54015106d
rohit <rohit.nandamudi@just-roar.com>
2017-06-27 15:01:46 +0530

    adding filter functionality to pool management

--- CtgCov/app/pool1-mang-page/pool1-mang-page.component.js
+++ CtgCov/app/pool1-mang-page/pool1-mang-page.component.js
@@ -10,8 +10,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/map");
 var Pool1MangPageComponent = (function () {
-    function Pool1MangPageComponent() {
+    function Pool1MangPageComponent(http) {
+        this.http = http;
+        this.selectedCompany = "Select a company";
+        this.selectedCsr = "Select a Csr";
+        this.selectedPlanner = "Select a planner";
+        //[name: string]: any;
+        this.allPlanners = [];
         this.name = 'Pool1Mang-pageComponent';
         this.cityFil = "";
         this.stateFil = "";
@@ -22,8 +30,25 @@ var Pool1MangPageComponent = (function () {
         this.countryList = ["AL", "AZ", "AR"];
         this.cityList = ["Birmingham", "Cullman", "Gentry", "North Little Rock", "Phoenix"];
         this.ccList = ["Averitt", "Serta", "Walmart 6008", "Valspar"];
-        this.config = {
-            rows: [{
+        this.allCities = [{ "cityName": "Cayuga", "cityState": "WI", "cityZip": "54546", "cityCountry": 3, "cityRegion": "UPMID", "cityLatitude": "46.24000", "cityLongtitude": "90.68000" }, { "cityName": "Cazenovia", "cityState": "WI", "cityZip": "53924", "cityCountry": 3, "cityRegion": "UPMID", "cityLatitude": "43.52000", "cityLongtitude": "90.20000" }];
+        this.allStates = [];
+        this.allCC = [];
+        this.poolToEdit = {
+            state: "",
+            city: "",
+            csr: "",
+            planner: "",
+            company: "",
+            reqPool: "",
+            curr: "",
+            variance: 0,
+            twm: "",
+            totReq: "",
+            toShow: true,
+            pool: ""
+        };
+        this.ob = {
+            groups: [{
                     state: "AL",
                     city: "Birmingham",
                     csr: "Mike",
@@ -118,6 +143,11 @@ var Pool1MangPageComponent = (function () {
             column: ["State", "City", "Pool #", "Company", "CSR", "Planner", "TMW", "Req Pool", "Current", "Variance", "Action"]
         };
         this.selectVarience(1);
+        this.getAllCities();
+        this.getAllStates();
+        this.getAllCompany();
+        this.getAllCsr();
+        this.getAllPlanner();
     }
     Pool1MangPageComponent.prototype.selectVarience = function (value) {
         this.data = [];
@@ -125,7 +155,7 @@ var Pool1MangPageComponent = (function () {
         console.log(this.cityFil + " " + this.stateFil);
         this.selectedVarience = value;
         // ... do other stuff here ...
-        for (var _i = 0, _a = this.config.rows; _i < _a.length; _i++) {
+        for (var _i = 0, _a = this.ob.groups; _i < _a.length; _i++) {
             var j = _a[_i];
             if (value == 0) {
                 if (j.variance == 0) {
@@ -151,6 +181,93 @@ var Pool1MangPageComponent = (function () {
             }
         }
     };
+    Pool1MangPageComponent.prototype.getAllCities = function () {
+        var _this = this;
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllCities").map(function (res) { return res.json(); })
+            .subscribe(function (data) { console.log("getAllCities data recieved"); _this.allCities = data; }, //For Success Response
+        function (//For Success Response
+            err) { console.error(err); } //For Error Response
+        );
+    };
+    Pool1MangPageComponent.prototype.getAllStates = function () {
+        var _this = this;
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllStates").map(function (res) { return res.json(); })
+            .subscribe(function (data) { console.log("getAllStates data recieved"); _this.allStates = data; }, //For Success Response
+        function (//For Success Response
+            err) { console.error(err); } //For Error Response
+        );
+    };
+    Pool1MangPageComponent.prototype.getAllCompany = function () {
+        var _this = this;
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllCompanies").map(function (res) { return res.json(); })
+            .subscribe(function (data) { console.log("getAllCompany data recieved"); _this.allCC = data; }, //For Success Response
+        function (//For Success Response
+            err) { console.error(err); } //For Error Response
+        );
+    };
+    Pool1MangPageComponent.prototype.getAllCsr = function () {
+        var _this = this;
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllCsr").map(function (res) { return res.json(); })
+            .subscribe(function (data) { console.log("getAllCsr data recieved"); _this.allCsr = data; }, //For Success Response
+        function (//For Success Response
+            err) { console.error(err); } //For Error Response
+        );
+    };
+    Pool1MangPageComponent.prototype.getAllPlanner = function () {
+        var _this = this;
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllPlanners").map(function (res) { return res.json(); })
+            .subscribe(function (data) { console.log("getAllPlanner data recieved"); _this.allPlanners = data; }, //For Success Response
+        function (//For Success Response
+            err) { console.error(err); } //For Error Response
+        );
+    };
+    Pool1MangPageComponent.prototype.toEdit = function (index) {
+        console.log(index);
+        this.poolToEdit = this.ob.groups[index];
+        this.selectedPlanner = this.poolToEdit.planner;
+        this.selectedCsr = this.poolToEdit.csr;
+        this.updateReqPool = this.poolToEdit.reqPool;
+    };
+    Pool1MangPageComponent.prototype.updatePool = function () {
+        this.poolToEdit.csr = this.selectedCsr;
+        this.poolToEdit.planner = this.selectedPlanner;
+        this.poolToEdit.reqPool = this.updateReqPool;
+    };
+    Pool1MangPageComponent.prototype.selectCompany = function (item) {
+        console.log(item);
+        this.selectedCompany = item;
+    };
+    Pool1MangPageComponent.prototype.selectPlanner = function (item) {
+        console.log(item);
+        this.selectedPlanner = item;
+    };
+    Pool1MangPageComponent.prototype.selectCsr = function (item) {
+        console.log(item);
+        this.selectedCsr = item;
+    };
+    Pool1MangPageComponent.prototype.add = function () {
+        //alert("a");
+        var ele = {
+            state: "",
+            city: "",
+            csr: "",
+            planner: "",
+            company: "",
+            reqPool: "",
+            curr: "",
+            variance: 0,
+            twm: "",
+            totReq: "",
+            toShow: true,
+            pool: ""
+        };
+        this.ob.groups.push(ele);
+    };
     return Pool1MangPageComponent;
 }());
 Pool1MangPageComponent = __decorate([
@@ -160,7 +277,7 @@ Pool1MangPageComponent = __decorate([
         styleUrls: ['pool1-mang-page.component.css'],
         moduleId: module.id,
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [http_1.Http])
 ], Pool1MangPageComponent);
 exports.Pool1MangPageComponent = Pool1MangPageComponent;
 // This code copy to app.module.ts
--- CtgCov/app/pool1-mang-page/pool1-mang-page.component.ts
+++ CtgCov/app/pool1-mang-page/pool1-mang-page.component.ts
@@ -6,11 +6,18 @@ import 'rxjs/add/operator/map';
 @Component({
     selector: 'pool1-mang-page',
     templateUrl: 'pool1-mang-page.component.html',
-    styleUrls: [ 'pool1-mang-page.component.css'],
+    styleUrls: ['pool1-mang-page.component.css'],
     moduleId: module.id,
     //pipes: [FilterCPipe]
 })
 export class Pool1MangPageComponent {
+    selectedCompany: any = "Select a company";
+    updateReqPool: any;
+    selectedCsr: any = "Select a Csr";
+    selectedPlanner: any = "Select a planner";
+    allCsr: any;
+    //[name: string]: any;
+    allPlanners: any = [];
     private name = 'Pool1Mang-pageComponent';
     cityFil: String = "";
     stateFil: String = "";
@@ -18,16 +25,30 @@ export class Pool1MangPageComponent {
     selectedVarience: Number = 1;
     data: any[] = [];
     pageNum: Number = 1;
-    countryList: any = ["AL","AZ","AR"];
+    countryList: any = ["AL", "AZ", "AR"];
     cityList: any = ["Birmingham", "Cullman", "Gentry", "North Little Rock", "Phoenix"];
     ccList: any = ["Averitt", "Serta", "Walmart 6008", "Valspar"];
     allCities: any = [{ "cityName": "Cayuga", "cityState": "WI", "cityZip": "54546", "cityCountry": 3, "cityRegion": "UPMID", "cityLatitude": "46.24000", "cityLongtitude": "90.68000" }, { "cityName": "Cazenovia", "cityState": "WI", "cityZip": "53924", "cityCountry": 3, "cityRegion": "UPMID", "cityLatitude": "43.52000", "cityLongtitude": "90.20000" }];
     allStates: any = [];
     allCC: any = [];
+    poolToEdit: any = {
+        state: "",
+        city: "",
+        csr: "",
+        planner: "",
+        company: "",
+        reqPool: "",
+        curr: "",
+        variance: 0,
+        twm: "",
+        totReq: "",
+        toShow: true,
+        pool: ""
+    };
     selectVarience(value: Number) {
         this.data = [];
         console.log(this.selectedVarience);
-        console.log(this.cityFil+" "+this.stateFil);
+        console.log(this.cityFil + " " + this.stateFil);
         this.selectedVarience = value;
         // ... do other stuff here ...
         for (var j of this.ob.groups) {
@@ -40,7 +61,7 @@ export class Pool1MangPageComponent {
                     j.toShow = false;
                 }
             } else if (value == 1) {
-                if (j.variance >0) {
+                if (j.variance > 0) {
                     j.toShow = true;
                     this.data.push(j);
                 } else {
@@ -144,31 +165,49 @@ export class Pool1MangPageComponent {
             twm: "VALBIRG02",
             totReq: "11",
             toShow: true,
-            pool:"WALBEN34"
+            pool: "WALBEN34"
         }],
-        column: ["State", "City", "Pool #", "Company", "CSR", "Planner","TMW", "Req Pool","Current","Variance","Action"]
+        column: ["State", "City", "Pool #", "Company", "CSR", "Planner", "TMW", "Req Pool", "Current", "Variance", "Action"]
     };
     private getAllCities() {
-        alert("hi");
+        //alert("hi");
         this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllCities").map(res => res.json())
             .subscribe(
-            (data) => { console.log(data); alert("Success"); this.allCities = data; }, //For Success Response
+            (data) => { console.log("getAllCities data recieved"); this.allCities = data; }, //For Success Response
             err => { console.error(err) } //For Error Response
-            );      
+            );
     }
     private getAllStates() {
-        alert("hi");
+        //alert("hi");
         this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllStates").map(res => res.json())
             .subscribe(
-            (data) => { console.log(data); alert("Success"); this.allStates = data; }, //For Success Response
+            (data) => { console.log("getAllStates data recieved"); this.allStates = data; }, //For Success Response
             err => { console.error(err) } //For Error Response
             );
     }
     private getAllCompany() {
-        alert("hi");
+        //alert("hi");
         this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllCompanies").map(res => res.json())
             .subscribe(
-            (data) => { console.log(data); alert("Success"); this.allCC = data; }, //For Success Response
+            (data) => { console.log("getAllCompany data recieved"); this.allCC = data; }, //For Success Response
+            err => { console.error(err) } //For Error Response
+            );
+    }
+
+    private getAllCsr() {
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllCsr").map(res => res.json())
+            .subscribe(
+            (data) => { console.log("getAllCsr data recieved"); this.allCsr = data; }, //For Success Response
+            err => { console.error(err) } //For Error Response
+            );
+    }
+
+    private getAllPlanner() {
+        //alert("hi");
+        this.http.get("http://192.168.1.77:81/TrailersCheck.asmx/GetAllPlanners").map(res => res.json())
+            .subscribe(
+            (data) => { console.log("getAllPlanner data recieved"); this.allPlanners = data; }, //For Success Response
             err => { console.error(err) } //For Error Response
             );
     }
@@ -178,14 +217,39 @@ export class Pool1MangPageComponent {
         this.getAllCities();
         this.getAllStates();
         this.getAllCompany();
+        this.getAllCsr();
+        this.getAllPlanner();
     }
 
-    
 
-    private addTrailer() {
 
+    private toEdit(index: any) {
+        console.log(index);
+        this.poolToEdit = this.ob.groups[index];
+        this.selectedPlanner = this.poolToEdit.planner;
+        this.selectedCsr = this.poolToEdit.csr;
+        this.updateReqPool = this.poolToEdit.reqPool;
     }
-    
+
+    private updatePool() {
+        this.poolToEdit.csr = this.selectedCsr;
+        this.poolToEdit.planner = this.selectedPlanner;
+        this.poolToEdit.reqPool = this.updateReqPool;
+    }
+
+    selectCompany(item: any) {
+        console.log(item);
+        this.selectedCompany = item;
+    }
+
+    selectPlanner(item: any) {
+        console.log(item);
+        this.selectedPlanner = item;
+    }
+    selectCsr(item: any) {
+        console.log(item);
+        this.selectedCsr = item;
+    }
     add() {
         //alert("a");
         var ele = {
@@ -203,9 +267,8 @@ export class Pool1MangPageComponent {
             pool: ""
         };
         this.ob.groups.push(ele);
-        
-
     }
+
 
 }
 
